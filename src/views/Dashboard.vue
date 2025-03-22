c7124625114575bbc5904f212c9a20fa84c35126
chheng <chheng@gmail.com>
2025-03-22 15:17:58 +0700

    modify the api ip address and add the academy file

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,18 @@ import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-8">
+    <!-- <div class="grid grid-cols-12 gap-8">
         <StatsWidget  />
+    </div> -->
+    <div>
+        <div class="card">
+            <h4 class=" font-black italic">Smart system, strong students — RUPP connects and empowers.</h4>
+            <h6>creating a smarter, more efficient academic environment. By streamlining tasks like enrollment, class
+                schedules, and grading, it allows students to focus more on learning and growth. This digital connection
+                between students, faculty, and administration builds a stronger, more supportive educational experience.
+            </h6>
+
+            <h6>more information comming soon ... </h6>
+        </div>
     </div>
 </template>
